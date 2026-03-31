94ba919198c834e4a5e80ce046182c523a131bd4
shreyas <shreyasmamtha19@gmail.com>
2026-03-31 19:59:39 +0530

    Update cloudinary.js

--- midguard-backend/utils/cloudinary.js
+++ midguard-backend/utils/cloudinary.js
@@ -1,9 +1,11 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary");
 
-cloudinary.config({
+// ✅ configure using v2 API
+cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
+// ✅ export FULL object (not .v2)
 module.exports = cloudinary;
