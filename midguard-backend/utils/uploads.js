7263a242782be89139a02a259f29e52c77ed5acf
shreyas <shreyasmamtha19@gmail.com>
2026-03-31 18:35:25 +0530

    Update uploads.js

--- midguard-backend/utils/uploads.js
+++ midguard-backend/utils/uploads.js
@@ -1,13 +1,12 @@
 const multer = require("multer");
-const path = require("path");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const unique = Date.now() + "-" + file.originalname;
-    cb(null, unique);
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "midguard",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
 
